71466bc929fe6e8fd68bbe72fa77051369bc9ad3
flej <sardor.ismoil.spam@gmail.com>
2025-11-15 20:38:27 -0500

    Change password hashing algorithm to sha256_crypt

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, request, jsonify
 from flask_cors import CORS
 import psycopg2
 import os, time, uuid
-from passlib.hash import bcrypt
+from passlib.hash import sha256_crypt as bcrypt
 
 app = Flask(__name__)
 CORS(app)
